43574f976b46e781a83aede8ecb258042324ed24
Tr1ckX <attila.mirk@gmail.com>
2014-10-30 11:59:28 +0000

    Ver_2 bike_container refactored

---
+++ Ver_1/lib/docking_station.rb
@@ -0,0 +1,31 @@
+class DockingStation
+  DEFAULT_CAPACITY = 20
+
+  def initialize(options = {})
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+    @bikes = []
+  end
+
+  def bike_count
+    @bikes.count
+  end
+
+  def dock(bike)
+    # if the capacity is reached, raise an error
+    raise "Station is full" if full?
+    @bikes << bike
+  end
+
+  def release(bike)
+    @bikes.delete(bike)
+  end
+
+  def full?
+    bike_count == @capacity
+  end
+
+  def available_bikes
+    @bikes.reject { |bike| bike.broken? }
+  end
+
+end
--- Ver_2/lib/docking_station.rb
+++ Ver_2/lib/docking_station.rb
@@ -1,31 +1,21 @@
+ #load BikeContainer
+ 
+ require_relative 'bike_container'
+
+
 class DockingStation
-  DEFAULT_CAPACITY = 20
+
+  # this gives us all the methods that used to be in this class
+  include BikeContainer
 
   def initialize(options = {})
-    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-    @bikes = []
-  end
-
-  def bike_count
-    @bikes.count
-  end
-
-  def dock(bike)
-    # if the capacity is reached, raise an error
-    raise "Station is full" if full?
-    @bikes << bike
-  end
-
-  def release(bike)
-    @bikes.delete(bike)
-  end
-
-  def full?
-    bike_count == @capacity
-  end
-
-  def available_bikes
-    @bikes.reject { |bike| bike.broken? }
+=begin
+    self.capacity is calling the capacity=() method (note the equals sign)
+    defined in BikeContainer
+    capacity (the second argument to fetch()) is calling the capacity() method in 
+    BikeContainer
+=end
+  self.capacity = options.fetch(:capacity, capacity)
   end
 
 end
